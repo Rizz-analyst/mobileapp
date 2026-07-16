676065a3832e2f67532a5611cfdbd63a129905c3
Rizwan <mohammedrizwan2650@gmail.com>
2026-07-17 02:05:17 +0530

    file added

---
+++ app2.js
@@ -0,0 +1,8 @@
+<html>
+  <body>
+  <h2> Javascript main page</h2>
+
+<p id="main"> javascript changes </p>
+<button type="button"onclick='document.getElementById("main").innerHTML ="Hey user welcome"'>submit </button>
+  </body>
+  </html>
